095dc7eab0cb6294edc18a3b575e9edf4d3571cd
David RF <davranfor@gmail.com>
2026-06-15 20:52:20 +0200

    Add no-store to all headers

--- server/include/headers.h
+++ server/include/headers.h
@@ -17,32 +17,40 @@
     "Cache-Control: no-store\r\n"
 
 #define HEADER_CREATED \
-    "HTTP/1.1 201 Created\r\n"
+    "HTTP/1.1 201 Created\r\n" \
+    "Cache-Control: no-store\r\n"
 
 #define HEADER_NO_CONTENT \
-    "HTTP/1.1 204 No Content\r\n"
+    "HTTP/1.1 204 No Content\r\n" \
+    "Cache-Control: no-store\r\n"
 
 #define HEADER_BAD_REQUEST \
-    "HTTP/1.1 400 Bad Request\r\n"
+    "HTTP/1.1 400 Bad Request\r\n" \
+    "Cache-Control: no-store\r\n"
 
 #ifdef ALLOW_INSECURE_TOKEN
 #define HEADER_UNAUTHORIZED \
     "HTTP/1.1 401 Unauthorized\r\n" \
+    "Cache-Control: no-store\r\n" \
     "Set-Cookie: session=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; HttpOnly; SameSite=Strict\r\n"
 #else
 #define HEADER_UNAUTHORIZED \
     "HTTP/1.1 401 Unauthorized\r\n" \
+    "Cache-Control: no-store\r\n" \
     "Set-Cookie: session=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; HttpOnly; SameSite=Strict\r\n"
 #endif
 
 #define HEADER_FORBIDDEN \
-    "HTTP/1.1 403 Forbidden\r\n"
+    "HTTP/1.1 403 Forbidden\r\n" \
+    "Cache-Control: no-store\r\n"
 
 #define HEADER_NOT_FOUND \
-    "HTTP/1.1 404 Not Found\r\n"
+    "HTTP/1.1 404 Not Found\r\n" \
+    "Cache-Control: no-store\r\n"
 
 #define HEADER_SERVER_ERROR \
-    "HTTP/1.1 500 Internal Server Error\r\n"
+    "HTTP/1.1 500 Internal Server Error\r\n" \
+    "Cache-Control: no-store\r\n"
 
 #endif
 
